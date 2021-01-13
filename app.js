cd23f39b3c4280051fc8edb6e36bd74e6870e694
geminerald <zacdwarner@gmail.com>
2021-01-13 12:51:57 +0000

    added basic update functionality

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("App connected")
+
+const inputName = document.getElementById('input-name');
+const cardName = document.getElementById('card-name');
+const updateBtn = document.getElementById('update');
+
+updateBtn.addEventListener('click',()=>{
+    cardName.innerHTML = inputName.value
+})
