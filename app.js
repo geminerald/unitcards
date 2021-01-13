8582db37b20753441d51e5ee69c7f32a7c826f0e
geminerald <zacdwarner@gmail.com>
2021-01-13 12:56:09 +0000

    added selectors to inputs

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 console.log("App connected")
 
 const inputName = document.getElementById('input-name');
+const inputImage = document.getElementById('input-image');
+const inputtype = document.getElementById('input-type');
+const inputcost = document.getElementById('input-cost');
+const inputAttack = document.getElementById('input-attack');
+const inputDefence = document.getElementById('input-defence');
+const inputPower = document.getElementById('input-power');
+const inputToughness = document.getElementById('input-toughness');
+const inputMorale = document.getElementById('input-morale');
+const inputSize = document.getElementById('input-size');
+const inputTraits = document.getElementById('input-traits');
+
+
+
+
 const cardName = document.getElementById('card-name');
+
 const updateBtn = document.getElementById('update');
+
+
+
+
+
 
 updateBtn.addEventListener('click',()=>{
     cardName.innerHTML = inputName.value
